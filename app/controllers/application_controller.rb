78cd4b8ac021faba1c12cbba4447a7d1e1092914
haruka <heily262626@gmail.com>
2024-03-29 07:53:05 +0900

    sign_inの時のpermitted parameter追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,12 @@ class ApplicationController < ActionController::Base
   
 	def configure_permitted_parameters
 	  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :ptj])
+	  devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
 	end
 
 	def after_sign_in_path_for(resource_or_scope)
 		member_path
 	end
 end
+
+# configure_permitted_parametersに関して、sing_upとsign_inのパラメータを許可している。
