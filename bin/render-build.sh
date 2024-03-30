ac0415cf29478944475cb1a9451229b508773e78
haruka <heily262626@gmail.com>
2024-03-30 15:19:27 +0900

    seed走らせるためrender-build.shの更新

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 bundle install
-bundle exec rails assets:precompile
+RAILS_ENV=production rails db:seed
 bundle exec rails assets:clean
+bundle exec rails assets:precompile
 bundle exec rails db:migrate
+bundle exec rails db:seed
