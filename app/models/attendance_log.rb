fc9800a27e744d6cca8afd372c74bfa0fcf76ec5
haruka <heily262626@gmail.com>
2024-03-31 11:11:30 +0900

    今月の出勤情報の雛形

--- app/models/attendance_log.rb
+++ app/models/attendance_log.rb
@@ -1,4 +1,13 @@
 class AttendanceLog < ApplicationRecord
 	belongs_to :user
 	enum attendance_type: { attendance: 0, break_start: 1, break_end: 2, leave: 3 }
+
+	def index
+		@attendance_logs = current_user.attendance_logs
+	end
+
+	def self.for_month(year, month)
+		where(created_at: "#{year}-#{month}-01".to_date.beginning_of_month.. "#{year}-#{month}-01".to_date.end_of_month)
+	end
+	
 end
