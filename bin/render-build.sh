32a6f735aeec16b854a7740411a3d4166b21808a
haruka <heily262626@gmail.com>
2024-04-01 17:53:07 +0900

    node version指定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 bundle install
 npm install
 export RAILS_ENV=production
+export NODE_OPTIONS=--openssl-legacy-provider
 bundle exec rails db:migrate
 bundle exec rails assets:clean
 bundle exec rails assets:precompile
