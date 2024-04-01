b15b59fa69ed7185ae44b2d8512f032d6a496ffb
haruka <heily262626@gmail.com>
2024-04-01 11:18:43 +0900

    打刻時間が入ってなかったところ修正、一度退勤まで行うと出勤二度通せなくなってた部分修正二度通せなくなってた部分修正

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,18 +2,28 @@ class AttendancesController < ApplicationController
 	before_action :authenticate_user!
   
 	def index
+		# 現在の年月を取得
 		@year = params[:year] || Time.zone.now.year
 		@month = params[:month] || Time.zone.now.month
-
-		# 特定の年月のログを取得し、created_atでソート
-		start_date = Date.new(@year.to_i, @month.to_i, 1)
-		end_date = start_date.end_of_month
-		@attendance_logs = current_user.attendance_logs.where(created_at: start_date..end_date).order(:created_at)
-	end
+		@attendance_logs = current_user.attendance_logs.for_month(@year, @month)
+		
+		# 先月の年月を計算
+		last_month_date = Time.zone.now.beginning_of_month.prev_month
+		last_month_year = last_month_date.year
+		last_month_month = last_month_date.month
+		@attendance_lm_logs = current_user.attendance_logs.for_month(last_month_year, last_month_month)
+		
+		@next_action = current_user.next_attendance_action
+	  end
 
 	def create
 		attendance_type = params[:attendance_type].to_i
-		current_user.attendance_logs.create(attendance_type: attendance_type, created_at: Time.zone.now) 
+		current_user.attendance_logs.create(
+			attendance_type: attendance_type, 
+			recorded_at: Time.zone.now,
+			created_at: Time.zone.now
+		)
+
 
 		if session[:retry] || current_user.attendance_logs.where("created_at >= ?", Date.today).empty?
 			# 出勤などの処理を行う
@@ -50,6 +60,11 @@ class AttendancesController < ApplicationController
 		render 'show_attendance', status: 303
 	end
 
+	def redo
+		session[:user_state] = nil
+		redirect_to member_path, notice: 'やり直しを実行しました。'
+	end
+
 	def undo
 		last_log = current_user.attendance_logs.order(created_at: :desc).first
 		if last_log && last_log.created_at.today?
@@ -60,14 +75,11 @@ class AttendancesController < ApplicationController
 		redirect_to member_path
 	end
 
-	def redo
-		redirect_to member_path, notice: 'やり直しを実行しました。'
-	  end
+	private
 
-	def retry
-		session[:retry] = true
-		redirect_to member_path, notice: "やり直しを開始できます。"
-	end
+	def attendance_log_params
+		params.permit(:attendance_type).transform_values(&:to_i)
+	end	  
 
 end
   
--- app/models/attendance_log.rb
+++ app/models/attendance_log.rb
@@ -2,10 +2,6 @@ class AttendanceLog < ApplicationRecord
 	belongs_to :user
 	enum attendance_type: { attendance: 0, break_start: 1, break_end: 2, leave: 3 }
 
-	def index
-		@attendance_logs = current_user.attendance_logs
-	end
-
 	def self.for_month(year, month)
 		where(created_at: "#{year}-#{month}-01".to_date.beginning_of_month.. "#{year}-#{month}-01".to_date.end_of_month)
 	end
